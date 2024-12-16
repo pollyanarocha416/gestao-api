bf0800ce433686c7c234dc7e8f8666b833cf1257
pollyanarocha416 <pollyanarocha416@gmail.com>
2024-12-16 19:41:04 -0400

    modal em fastapi

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import models, db
 from config import engine
+from enum import Enum
 
 
 models.Banco.metadata.create_all(bind=engine)
@@ -52,10 +53,30 @@ def delete_item(item_id: int,
             db: Session = Depends(db.conection)):
 
     db_product = db.query(models.Products).filter(models.Products.id == item_id).first()
-    
+
     if db_product is None:
         raise HTTPException(status_code=404, detail="Item not found")
     db.delete(db_product)
     db.commit()
 
     return {"detail": "Item deleted"}
+
+
+# teste
+
+
+class ModelFuncionarios(str, Enum):
+    joao = "joao"
+    mateus = "mateus"
+    julia = "julia"
+
+
+@app.get("/models/{model_funcionarios}")
+async def get_model(model_funcionarios: ModelFuncionarios):
+    if model_funcionarios is ModelFuncionarios.joao:
+        return {"model_name": model_funcionarios, "message": "TI"}
+
+    if model_funcionarios.value == "joao":
+        return {"model_name": model_funcionarios, "message": "TI Senior"}
+
+    return {"model_funcionarios": model_funcionarios, "message": "Outra area"}
