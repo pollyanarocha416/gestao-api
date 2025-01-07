b93d3c290521fd975c3f4f7ed77f4f2e1a9b59b6
pollyanarocha416 <pollyanarocha416@gmail.com>
2025-01-07 19:12:25 -0400

    adicionando expressao regular

--- main.py
+++ main.py
@@ -137,6 +137,7 @@ def delete_item(item_id: int, db: Session = Depends(db.conection)):
 
 @app.get("/teste/")
 async def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=50)):
+    # , pattern="^fixedquery$"  -> caso queira adicionar uma expreção regular
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
